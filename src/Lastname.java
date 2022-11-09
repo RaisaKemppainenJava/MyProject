a62050c07fdeac8c521771aa94b013c8f3f379d9
RaisaKemppainen <raisa.kemppainen@brightstraining.com>
2022-11-09 13:25:26 +0100

    Added Class in bransh and called a new method in Main

---
+++ src/Lastname.java
@@ -0,0 +1,6 @@
+public class Lastname {
+	public void lastName(){
+		String myLastNameIs = "Kemppainen";
+		System.out.println("My lastname is " + myLastNameIs);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 
 	static Name myName = new Name();
+	static Lastname myLastname = new Lastname();
 
 	public static void main(String[] args) {
 		System.out.println("Hi Git and Github");
 		myName.name();
+		myLastname.lastName();
 	}
 }
